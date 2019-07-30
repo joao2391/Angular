fb6c1ec480d8b127815d3bf1e206ca13a8141156
joaopaulo2391 <j_paulo_15@hotmail.com>
2019-07-30 20:47:27 -0300

    Init Project

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MeuComponenteComponent } from './meu-componente/meu-componente.component';
+import { MeuComponente2Component } from './meu-componente2/meu-componente2.component'; 
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MeuComponenteComponent,
+    MeuComponente2Component
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/meu-componente/meu-componente.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'meu-componente-component',
+    template: '<div>Meu Componente</div>'
+})
+
+export class MeuComponenteComponent{
+    
+}
